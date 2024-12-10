bddb6d44830a3e06f230eb1f386a7f242e772303
Daffarel Adyatma Winuradmaja <farelalden96@gmail.com>
2024-12-10 22:21:09 +0700

    fixed layout for searchresult

--- app/src/main/java/com/example/sportify/layout_component/FloatingCartButton.kt
+++ app/src/main/java/com/example/sportify/layout_component/FloatingCartButton.kt
@@ -2,6 +2,7 @@ package com.example.sportify.layout_component
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.FloatingActionButton
@@ -11,15 +12,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun FloatingCartButton(onClick: () -> Unit) {
-    Box(){
+fun FloatingCartButton(onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.padding(16.dp)
+    ) {
         FloatingActionButton(
-            onClick =  onClick,
-            Modifier
-                .align(Alignment.BottomEnd)
+            onClick = onClick,
+            modifier = Modifier.align(Alignment.BottomEnd)
         ) {
             Icon(
                 imageVector = Icons.Filled.ShoppingCart,
